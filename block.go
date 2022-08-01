17e809d3eef6af5ada13edaa08ae4133c786a9ec
asstart <asst.artem@gmail.com>
2022-08-02 02:43:46 +0300

    Added block type objects

--- block.go
+++ block.go
@@ -18,37 +18,37 @@ type Block struct {
 	Archived       *bool        `json:"archived,omitempty"`
 	HasChildred    *bool        `json:"has_children,omitempty"`
 
-	Paragraph        interface{} `json:"paragraph,omitempty"`
-	Heading_1        interface{} `json:"heading_1,omitempty"`
-	Heading_2        interface{} `json:"heading_2,omitempty"`
-	Heading_3        interface{} `json:"heading_3,omitempty"`
-	Callout          interface{} `json:"callout,omitempty"`
-	Quote            interface{} `json:"quote,omitempty"`
-	BulletedListItem interface{} `json:"bulleted_list_item,omitempty"`
-	NumberedListItem interface{} `json:"numbered_list_item,omitempty"`
-	ToDo             interface{} `json:"to_do,omitempty"`
-	Toggle           interface{} `json:"toggle,omitempty"`
-	Code             interface{} `json:"code,omitempty"` //WTF
-	ChildPage        interface{} `json:"child_page,omitempty"`
-	ChildDatabase    interface{} `json:"child_database,omitempty"`
-	Embed            interface{} `json:"embed,omitempty"`
-	Image            interface{} `json:"image,omitempty"`
-	Video            interface{} `json:"video,omitempty"`
-	File             interface{} `json:"file,omitempty"`
-	Pdf              interface{} `json:"pdf,omitempty"`
-	Bookmark         interface{} `json:"bookmark,omitempty"`
-	Equation         interface{} `json:"equation,omitempty"`
-	Divider          interface{} `json:"divider,omitempty"`
-	TableOfContents  interface{} `json:"table_of_contents,omitempty"` // Table of contents block?
-	BreadCrumbBlocks interface{} `json:"breadcrumb_blocks,omitempty"` //WTF
-	ColumnList       interface{} `json:"column_list,omitempty"`       //Column Block?
-	LinkPreview      interface{} `json:"link_preview,omitempty"`      //Link Privew blocks?
-	Template         interface{} `json:"template,omitempty"`          //Tempalte Blocks?
-	LinkToPage       interface{} `json:"link_to_page,omitempty"`      //Link To page BlocK?
-	Table_row        interface{} `json:"table_row,omitempty"`
-	SyncedBlock      interface{} `json:"synced_block,omitempty"`
-	Column           interface{} `json:"column,omitempty"` //row?
-	Table            interface{} `json:"table,omitempty"`
+	Paragraph        *TextBlock            `json:"paragraph,omitempty"`
+	Heading_1        *HeadingBlock         `json:"heading_1,omitempty"`
+	Heading_2        *HeadingBlock         `json:"heading_2,omitempty"`
+	Heading_3        *HeadingBlock         `json:"heading_3,omitempty"`
+	Callout          *CalloutBlock         `json:"callout,omitempty"`
+	Quote            *TextBlock            `json:"quote,omitempty"`
+	BulletedListItem *TextBlock            `json:"bulleted_list_item,omitempty"`
+	NumberedListItem *TextBlock            `json:"numbered_list_item,omitempty"`
+	ToDo             *TodoBlock            `json:"to_do,omitempty"`
+	Toggle           *TextBlock            `json:"toggle,omitempty"`
+	Code             *CodeBlock            `json:"code,omitempty"` //WTF
+	ChildPage        *ChildBlock           `json:"child_page,omitempty"`
+	ChildDatabase    *ChildBlock           `json:"child_database,omitempty"`
+	Embed            *EmbedBlock           `json:"embed,omitempty"`
+	Image            *ImageBlock           `json:"image,omitempty"`
+	Video            *VideoBlock           `json:"video,omitempty"`
+	File             *FileBlock            `json:"file,omitempty"`
+	Pdf              *PDFBlock             `json:"pdf,omitempty"`
+	Bookmark         *BookmarkBlock        `json:"bookmark,omitempty"`
+	Equation         *EquationBlock        `json:"equation,omitempty"`
+	Divider          *DividerBlock         `json:"divider,omitempty"`
+	TableOfContents  *TableOfContentsBlock `json:"table_of_contents,omitempty"` // Table of contents block?
+	BreadCrumbBlocks *BreadCrumbBlock      `json:"breadcrumb_blocks,omitempty"` //WTF
+	ColumnList       *ColumnListBlock      `json:"column_list,omitempty"`       //Column Block?
+	Column           *ColumnBlock          `json:"column,omitempty"`            //row?
+	LinkPreview      *LinkPreviewBlock     `json:"link_preview,omitempty"`      //Link Privew blocks?
+	Template         *TemplateBlock        `json:"template,omitempty"`          //Tempalte Blocks?
+	LinkToPage       *LinkToPageBlock      `json:"link_to_page,omitempty"`      //Link To page BlocK?
+	SyncedBlock      *SyncedBlock          `json:"synced_block,omitempty"`
+	Table            *TableBlock           `json:"table,omitempty"`
+	TableRow         *TableRowBlock        `json:"table_row,omitempty"`
 }
 
 type BlockParent struct {
@@ -221,4 +221,141 @@ func (p BlockType) MarshalJSON() ([]byte, error) {
 	b.WriteString(BlockTypeToString[p])
 	b.WriteString(`"`)
 	return b.Bytes(), nil
+}
+
+type TextBlock struct {
+	RichText []RichText `json:"rich_text"`
+	Color    string     `json:"color,omitempty"` //TODO colors in tags has lower count of available values or not? Check and make single color enum
+	Children []Block    `json:"children,omitempty"`
+}
+
+type HeadingBlock struct {
+	RichText []RichText `json:"rich_text"`
+	Color    string     `json:"color,omitempty"` //TODO colors in tags has lower count of available values or not? Check and make single color enum
+}
+
+type CalloutBlock struct {
+	RichText []RichText      `json:"rich_text"`
+	Icon     *IconDescriptor `json:"icon,omitempty"`
+	Color    string          `json:"color,omitempty"` //TODO colors in tags has lower count of available values or not? Check and make single color enum
+	Children []Block         `json:"children,omitempty"`
+}
+
+type TodoBlock struct {
+	RichText []RichText `json:"rich_text"`
+	Checked  bool       `json:"checked"`
+	Color    string     `json:"color,omitempty"` //TODO colors in tags has lower count of available values or not? Check and make single color enum
+	Children []Block    `json:"children,omitempty"`
+}
+
+type CodeBlock struct {
+	RichText []RichText `json:"rich_text"`
+	Caption  []RichText `json:"caption"`
+	Language string     `json:"color,omitempty"` //TODO add language enum
+}
+
+type ChildBlock struct {
+	Title string `json:"title"`
+}
+
+/*
+Embed blocks created via Notion API may differ of embed blocks created via UI
+Details: https://developers.notion.com/reference/block#embed-blocks
+*/
+type EmbedBlock struct {
+	Url string `json:"url"`
+}
+
+type ImageBlock struct {
+	Image FileDescriptor `json:"image"`
+}
+
+type VideoBlock struct {
+	Video FileDescriptor `json:"image"`
+}
+
+type FileBlock struct {
+	File FileDescriptor `json:"file"`
+}
+
+type PDFBlock struct {
+	PDF FileDescriptor `json:"pdf"`
+}
+
+type BookmarkBlock struct {
+	URL     string `json:"url"`
+	Caption string `json:"caption"`
+}
+
+type EquationBlock struct {
+	Expression string `json:"expression"`
+}
+
+type DividerBlock struct{}
+
+type TableOfContentsBlock struct {
+	Color string `json:"color,omitempty"` //TODO colors in tags has lower count of available values or not? Check and make single color enum
+}
+
+type BreadCrumbBlock struct{}
+
+/*
+TODO need to test ColumnListBlock and ColumnBlock carefuly https://developers.notion.com/reference/block#column-list-and-column-blocks
+*/
+type ColumnListBlock struct {
+	Children []ColumnBlock `json:"children"`
+}
+
+/*
+Can contains any block type itself "column", need validation
+*/
+type ColumnBlock struct {
+	Children []Block `json:"children"`
+}
+
+/*
+Link Preview block objects return the originally pasted url.
+NOTE: The link_preview block will only be returned as part of a response.
+It cannot be created via the API.
+https://developers.notion.com/reference/block#link-preview-blocks
+*/
+type LinkPreviewBlock struct {
+	URL string `json:"link_preview"`
+}
+
+type TemplateBlock struct {
+	RichText []RichText `json:"rich_text"`
+	Children []Block    `json:"children"`
+}
+
+type LinkToPageBlock struct {
+	Type       string `json:"type"` //TODO need to create enum
+	PageID     string `json:"page_id,omitempty"`
+	DatabaseID string `json:"database_id,omitempty"`
+}
+
+/*
+Need validations to prevent creating original synced block with filled SyncedFrom and visa verse
+Details: https://developers.notion.com/reference/block#synced-block-blocks
+*/
+type SyncedBlock struct {
+	SyncedFrom *SyncedFrom `json:"synced_from,omitempty"`
+	Children   []Block     `json:"children"`
+}
+
+type SyncedFrom struct {
+	Type    string `json:"type"` //TODO the only possible value now is block, but might need to create enum
+	BlockID string `json:"block_id"`
+}
+
+type TableBlock struct {
+	//Note that this cannot be changed via the public API once a table is created.
+	TableWidth      int             `json:"table_width"`
+	HasColumnHeader bool            `json:"has_column_header"`
+	HasRowHeader    bool            `json:"has_row_header"`
+	Children        []TableRowBlock `json:"children"`
+}
+
+type TableRowBlock struct {
+	Cells []RichText `json:"cells"`
 }
